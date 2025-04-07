992ba72b69578eaa7481c70fd9cf3bf888737748
GaneshramWebdev <gram00552@gmail.com>
2025-04-08 00:26:06 +0530

    app-write error-fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/Search";
 import { Spinner } from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
-import { getTrendingMovies,updateSearchCount } from "./DB";
+import { getTrendingMovies,updateSearchCount } from "./appWrite";
 
 const API_Base_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
