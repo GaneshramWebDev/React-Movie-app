428bdc1b50c8483406dfba9c86daa3d72ec2cc9a
GaneshramWebdev <gram00552@gmail.com>
2025-04-08 00:06:03 +0530

    appwrite edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/Search";
 import {Spinner } from "./components/Spinner"
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
-import { getTrendingMovies, updateSearchCount } from "./appWrite";
+import { getTrendingMovies, updateSearchCount } from "./appWrite"
 
 
 const API_Base_URL = "https://api.themoviedb.org/3";
